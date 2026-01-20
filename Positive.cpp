72cdf1fb4de753178b9586442beec31c576372a0
Priyan90 <priyanshuj662@gmail.com>
2026-01-20 12:14:33 +0530

    Notes

---
+++ Positive.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n ;
+cin>>n;
+ if (n==0){
+        cout<<"Zero"<<endl;
+    }
+else{
+if(n>=0){
+    cout<<"Positive "<<endl;
+}
+else{
+    cout<<"Negative "<<endl;   
+}
+}
+}
